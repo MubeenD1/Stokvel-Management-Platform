355741ffdd7115000d15e075cf822ba8f888255a
Simphiwe Kwela <simzjunioracc@gmail.com>
2026-04-12 23:30:56 +0200

    added create group feature

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -39,7 +39,7 @@ export default function App() {
             </ProtectedRoute>
           }
         />
-        <Route path="*" element={<Navigate to="/dashboard" />} />
+        <Route path="*" element={<Navigate to="/login" />} />
       </Routes>
     </BrowserRouter>
   );
