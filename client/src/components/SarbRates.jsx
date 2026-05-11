92865ce4ff66e9707c8913ea5ff546e6ba626801
Thabiso-Thuso <ramaemathuso@gmail.com>
2026-05-11 11:33:59 +0200

    Update API URL to use environment variable

--- client/src/components/SarbRates.jsx
+++ client/src/components/SarbRates.jsx
@@ -11,7 +11,8 @@ function SarbRates() {
             try {
                 const token = await auth.currentUser.getIdToken();
 
-                const response = await fetch('http://localhost:3000/api/sarb/rates', {
+                //const response = await fetch('http://localhost:3000/api/sarb/rates', {
+                    const res = await fetch(import.meta.env.VITE_API_URL + `/api/sarb/rates`, {
                     headers: {
                         'Authorization': `Bearer ${token}`,
                     },
