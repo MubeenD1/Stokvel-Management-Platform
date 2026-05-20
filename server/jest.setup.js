706dc6215ffb7b48373acc4fae908a1d924dc612
Simphiwe Kwela <simzjunioracc@gmail.com>
2026-05-20 21:05:43 +0200

    Create jest.setup.js

---
+++ server/jest.setup.js
@@ -0,0 +1,5 @@
+globalThis.console.log = () => {};
+globalThis.console.error = () => {};
+globalThis.console.warn = () => {};
+globalThis.console.info = () => {};
+globalThis.console.debug = () => {};
