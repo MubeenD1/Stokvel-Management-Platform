b2a7913af851a203a40f0d6277996128d0c23efa
Thabiso-Thuso <ramaemathuso@gmail.com>
2026-05-21 18:20:49 +0200

    Reverted to old config

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,15 +1,13 @@
 import { initializeApp } from 'firebase/app'
 import { getAuth, GoogleAuthProvider } from 'firebase/auth'
-
 const firebaseConfig = {
-  apiKey: "AIzaSyAKyQM4RKVbT5W6ObdwY_sOZDMNunhhkCY",
-  authDomain: "stokvel-management-platf-f0977.firebaseapp.com",
-  projectId: "stokvel-management-platf-f0977",
-  storageBucket: "stokvel-management-platf-f0977.firebasestorage.app",
-  messagingSenderId: "386384491583",
-  appId: "1:386384491583:web:51dbdb748fc1f7ae7db48a"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 }
-
 const app = initializeApp(firebaseConfig);
 const googleProvider = new GoogleAuthProvider();
 const auth = getAuth(app);
