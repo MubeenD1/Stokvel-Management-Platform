638d85c2cae9f6473edff6a5316261a12f7edf9d
Thabiso-Thuso <ramaemathuso@gmail.com>
2026-05-21 17:48:47 +0200

    Fix missing closing quote in projectId

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -4,7 +4,7 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth'
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "stokvel-management-platf-f0977.firebaseapp.com",
-  projectId: "stokvel-management-platf-f0977,
+  projectId: "stokvel-management-platf-f0977",
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID
